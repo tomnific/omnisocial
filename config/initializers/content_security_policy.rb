ecf1e8a97073b98ff5860e6dd1b216743d3f64cb
sojrner <sojrner@gmail.com>
2022-12-14 00:14:12 -0700

    CSP Corrections
    Further content security policy fixes... found the issue with the SVG images as well, proved it with the wordmark here, will update others tomorrow. Existing errors in console happen with original images as well. (tested with historical working files, then proved it with wordmark updated file) Does not appear to actually stop the image from loading, as it's a style error, not image.

--- config/initializers/content_security_policy.rb
+++ config/initializers/content_security_policy.rb
@@ -20,9 +20,9 @@ Rails.application.config.content_security_policy do |p|
   p.base_uri        :none
   p.default_src     :none
   p.frame_ancestors :none
-  p.font_src        :self, assets_host, "https://use.typekit.net/af/d613fb/00000000000000007735eecd/30/l?subset_id=2&fvd=n3&v=3", "https://use.typekit.net/af/d613fb/00000000000000007735eecd/30/d?subset_id=2&fvd=n3&v=3", "https://use.typekit.net/af/d613fb/00000000000000007735eecd/30/a?subset_id=2&fvd=n3&v=3", "https://use.typekit.net/af/ac9a0f/00000000000000007735eece/30/l?primer=7cdcb44be4a7db8877ffa5c0007b8dd865b3bbc383831fe2ea177f62257a9191&fvd=n4&v=3", "https://use.typekit.net/af/ac9a0f/00000000000000007735eece/30/d?primer=7cdcb44be4a7db8877ffa5c0007b8dd865b3bbc383831fe2ea177f62257a9191&fvd=n4&v=3", "https://use.typekit.net/af/ac9a0f/00000000000000007735eece/30/a?primer=7cdcb44be4a7db8877ffa5c0007b8dd865b3bbc383831fe2ea177f62257a9191&fvd=n4&v=3"
-  p.img_src         :self, :https, :data, :blob, assets_host
-  p.style_src       :self, assets_host, "https://use.typekit.net/vgd0zsc.css", "https://p.typekit.net/p.css?s=1&k=vgd0zsc&ht=tk&f=35655.35657&a=2943358&app=typekit&e=css"
+  p.font_src        :self, :https, :data, assets_host, "https://use.typekit.net"
+  p.img_src         :self, :unsafe_inline, :https, :data, :blob, assets_host
+  p.style_src       :self, assets_host, :data, :blob, "https://use.typekit.net", "https://p.typekit.net"
   p.media_src       :self, :https, :data, assets_host
   p.frame_src       :self, :https
   p.manifest_src    :self, assets_host
